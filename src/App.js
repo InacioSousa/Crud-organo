12814260a503ebb16ce8c99592bac642f4f998a4
InacioSousa <franciscojosesousas@gmail.com>
2022-12-05 13:39:32 -0300

    Construindo o Formulario

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 
 import './App.css';
 import Banner from './componentes/Banner/Banner'
-import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/Formulario';
+
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto  label="Nome" placeholder="Digite seu nome"/>
-      <CampoTexto  label="Cargo" placeholder="Digite seu Cargo"/>
-      <CampoTexto  label="Imagem" placeholder="Insira sua Imagem"/>
+      <Formulario />
+      
     </div>
   );
 }
