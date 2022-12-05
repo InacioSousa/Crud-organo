a333ff8434a15577ec962f6de37fe54cccf31dcc
InacioSousa <franciscojosesousas@gmail.com>
2022-12-05 12:34:04 -0300

    Ajustes no formulário

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+function banner(){
+    return(
+        <header className='banner'>
+    <img src="/imagens/banner.png" alt="imagem do banner"/>
+    </header>
+    )
+}
+
+export default banner;
